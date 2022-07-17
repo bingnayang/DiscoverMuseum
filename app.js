95cf2e758db865f722f8b14ef507b09e5b170420
BingNa Yang <bing334@gmail.com>
2022-07-16 22:14:19 -0400

    Update app.js to connect mongoose and check for error

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Connect mongoose and check for error
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+// Using template engines with Express
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'))
 
@@ -9,6 +23,7 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+// Listen to port 3000
 app.listen(3000, () => {
     console.log("Serving on port 3000")
 })
