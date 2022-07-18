9e8b976dbbd0b64bc49d7fdd73138ca9024694a7
BingNa Yang <bing334@gmail.com>
2022-07-17 20:49:45 -0400

    fixed typo, add GET museum route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
+const Museum = require('./models/museum');
 
 // Connect mongoose and check for error
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+mongoose.connect('mongodb://localhost:27017/discover-museum',{
     // useNewUrlParser: true,
     // useCreateIndex: true,
     // useUnifiedTopology: true
@@ -21,6 +23,12 @@ app.set('views', path.join(__dirname,'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+// Museum route: GET /museums
+app.get('/museums', async (req, res) => {
+    const museums = await Museum.find({});
+    res.render('museums/index', {museums});
 })
 
 // Listen to port 3000
